cb4c2be110744e3c7ddcdd72645c5436291f6822
luozhixin6 <1761316350@qq.com>
2019-09-25 11:15:03 +0800

    test

---
+++ lzx/src/SuanFa/test123456.java
@@ -0,0 +1,13 @@
+package SuanFa;
+
+/**
+ * @Auther:zhixin.Luo
+ * @Date: 2019/9/25 11:13
+ * @Description:
+ */
+public class test123456 {
+
+    public static void main(String[] args) {
+        System.out.println("6666666");
+    }
+}
