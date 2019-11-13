b68553299a78b49001b007833037a8600d65afc8
luozhixin6 <1761316350@qq.com>
2019-11-13 17:37:52 +0800

    20191113

---
+++ lzx/src/tcs/greedySnackMain.java
@@ -0,0 +1,30 @@
+package tcs;/**
+ * @Auther:zhixin.Luo
+ * @Date: 2019/11/13 16:12
+ * @Description:
+ */
+
+import javax.swing.JFrame;
+
+public class greedySnackMain extends JFrame {
+    snackWin snackwin;
+    static final int Width = 800 , Height = 600 , LocX = 10 , LocY = 100;
+
+    public greedySnackMain() {
+        super("GreedySncak_SL");
+        snackwin = new snackWin();
+        //在JFrame窗口容器里添加其他组件
+        add(snackwin);
+        //设置组件的大小
+        this.setSize(Width, Height);
+        //设置组件的可见性
+        this.setVisible(true);
+        //设置组件的位置
+        this.setLocation(LocX, LocY);
+        //snackwin.requestFocus();
+    }
+
+    public static void main(String[] args) {
+        new greedySnackMain();
+    }
+}
